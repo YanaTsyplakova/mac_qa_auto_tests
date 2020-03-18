9fc05269fefd37578950a39b5516ff01195a576e
Yana Tsyplakova <efimenko.yana11@yandex.ru>
2020-03-18 22:37:25 +0300

    Создала pages и вынесла туда переменные

---
+++ pages/wysiwygPage.js
@@ -0,0 +1,9 @@
+const { I } = inject();
+
+module.exports = {
+
+  button: {
+    field: '#tinymce',
+    iframe: '#mce_0_ifr',
+  }
+}
--- wysiwygEditor_test.js
+++ wysiwygEditor_test.js
@@ -1,18 +1,20 @@
 Feature('Editor');
+const wysiwyg = require('./pages/wysiwygPage')
 
 Scenario('Работа с WYSIWYG Editor', (I) => {
     I.amOnPage('http://the-internet.herokuapp.com/');
     I.click('WYSIWYG Editor');
-    I.switchTo('#mce_0_ifr');
-    I.click('#tinymce');
+    I.switchTo(wysiwyg.button.iframe);
+    I.click(wysiwyg.button.field);
     I.pressKey(['CommandOrControl', 'A']);
-    I.clearField('#tinymce');
-    I.fillField('#tinymce','АААААА');
+    I.clearField(wysiwyg.button.field);
+    I.fillField(wysiwyg.button.field,'АААААА');
     I.switchTo();
     I.click('.mce-i-bold');
-    I.switchTo('#mce_0_ifr');
-    I.fillField('#tinymce','MMMMMMM');
+    I.switchTo(wysiwyg.button.iframe);
+    I.fillField(wysiwyg.button.field,'MMMMMMM');
     I.saveScreenshot('really_bold_test.png');
+
 
 
 
